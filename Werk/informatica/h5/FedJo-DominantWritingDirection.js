610f219f28199427a6bfd307d3b557754870d487
TheFedJo <52329960+TheFedJo@users.noreply.github.com>
2020-02-06 15:11:06 +0100

    Update FedJo-DominantWritingDirection.js

--- Werk/informatica/h5/FedJo-DominantWritingDirection.js
+++ Werk/informatica/h5/FedJo-DominantWritingDirection.js
@@ -1,16 +1,27 @@
 function dominantDirection(text) {
+  let nltr = 0;
+  let nrtl = 0;
+  let nttb = 0;
   for (let element of text) {
-    if (characterScript(element).direction === "ltr") {console.log("ltr");}
-    
+    if (!(
+      characterScript(element.codePointAt()) === null 
+      || characterScript(element.codePointAt()) === undefined
+    )){
+      if (characterScript(element.codePointAt()).direction === "ltr") {
+        nltr++;        
+      } else if (characterScript(element.codePointAt()).direction === "rtl") {
+        nrtl++;        
+      } else {nttb++;}
+    }
   }
-  
-  
-  
-  
-  
+  if (nltr > nttb && nltr > nrtl) {
+    return "ltr"
+  } else if (nttb > nrtl) {
+    return "ttb"
+  } else {return "rtl"} 
 }
 
-console.log(dominantDirection("Hello!"));
+console.log(dominantDirection("HALLO"));
 // → ltr
 console.log(dominantDirection("Hey, مساء الخير"));
 // → rtl
