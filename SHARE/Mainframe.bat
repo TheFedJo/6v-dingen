e32d9b04d98ecfcfdfe1e6d8559cbaa80a95fea4
TheFedJo <52329960+TheFedJo@users.noreply.github.com>
2020-02-14 09:23:35 +0100

    Create Mainframe.bat

---
+++ SHARE/Mainframe.bat
@@ -0,0 +1,28 @@
+cls
+@echo off
+color 0a
+echo You've entered the mainframe 
+echo Please specify your next action
+timeout /t 1 /nobreak >NUL
+set /P user_input=Do yuo want to hack the mainframe?[Y/N] >NUL
+timeout /t 1 /nobreak >NUL
+echo Loading...
+timeout /t 1 /nobreak >NUL
+echo 34%%
+timeout /t 1 /nobreak >NUL
+echo 69%%
+timeout /t 1 /nobreak >NUL
+echo 98%%
+timeout /t 1 /nobreak >NUL
+echo 100%%
+echo.
+echo.
+echo.
+timeout /t 1 /nobreak >NUL
+echo The current session has been aborted. An unknown entity has denied access to the mainframe. Please contact an administrator if this should not be happening.
+echo Press any key to continue...
+pause >NUL
+timeout /t 1 /nobreak >NUL
+echo Shutting down...
+timeout /t 1 /nobreak >NUL
+exit
