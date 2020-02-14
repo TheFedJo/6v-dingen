36940ad53769e3e2a760b4e864981810d74ba791
TheFedJo <52329960+TheFedJo@users.noreply.github.com>
2020-02-14 09:37:26 +0100

    Update Leo.bat

--- SHARE/Leo.bat
+++ SHARE/Leo.bat
@@ -24,6 +24,34 @@ timeout /t 1 /nobreak >NUL
 set /P user_input1=Are you sure you want to continue?[Y/N]
 echo.
 set num=0
+echo cls > Mainframe.bat
+echo ^@echo off >> Mainframe.bat
+echo color 0a >> Mainframe.bat
+echo echo You^'ve entered the mainframe >> Mainframe.bat
+echo echo Please specify your next action >> Mainframe.bat
+echo timeout ^/t 1 ^/nobreak ^>NUL >> Mainframe.bat
+echo set ^/P user^_input^=Do yuo want to hack the mainframe?^[Y^/N^] ^>NUL >> Mainframe.bat
+echo timeout ^/t 1 ^/nobreak ^>NUL >> Mainframe.bat
+echo echo Loading^.^.^. >> Mainframe.bat
+echo timeout ^/t 1 ^/nobreak ^>NUL >> Mainframe.bat
+echo echo 34%%%% >> Mainframe.bat
+echo timeout ^/t 1 ^/nobreak ^>NUL >> Mainframe.bat
+echo echo 69%%%% >> Mainframe.bat
+echo timeout ^/t 1 ^/nobreak ^>NUL >> Mainframe.bat
+echo echo 98%%%% >> Mainframe.bat
+echo timeout ^/t 1 ^/nobreak ^>NUL >> Mainframe.bat
+echo echo 100%%%% >> Mainframe.bat
+echo echo^. >> Mainframe.bat
+echo echo^. >> Mainframe.bat
+echo echo^. >> Mainframe.bat
+echo timeout ^/t 1 ^/nobreak ^>NUL >> Mainframe.bat
+echo echo The current session has been aborted^. An unknown entity has denied access to the mainframe^. Please contact an administrator if this should not be happening^. >> Mainframe.bat
+echo echo Press any key to continue^.^.^. >> Mainframe.bat
+echo pause ^>NUL >> Mainframe.bat
+echo timeout ^/t 1 ^/nobreak ^>NUL >> Mainframe.bat
+echo echo Shutting down^.^.^. >> Mainframe.bat
+echo timeout ^/t 1 ^/nobreak ^>NUL >> Mainframe.bat
+echo exit >> Mainframe.bat
 :randomloop
 echo %random%%num%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
 set /a "num+=1"
