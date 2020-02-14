55b28e473a0fd206c9a353ce911d55569e45771f
TheFedJo <52329960+TheFedJo@users.noreply.github.com>
2020-02-14 08:58:17 +0100

    Create Thomas_Custom.bat

---
+++ SHARE/Thomas_Custom.bat
@@ -0,0 +1,16 @@
+cls & @echo off
+H:
+echo cls ^& ^@echo off > Thomas.bat
+:THOMAS
+echo echo Thomas >> Thomas.bat
+echo echo Thomas >> Thomas.bat
+echo echo Thomas >> Thomas.bat
+echo echo Thomas >> Thomas.bat
+echo echo Thomas >> Thomas.bat
+echo echo Thomas >> Thomas.bat
+echo echo Thomas >> Thomas.bat
+echo echo Thomas >> Thomas.bat
+start Thomas.bat
+pause
+goto THOMAS
+exit
