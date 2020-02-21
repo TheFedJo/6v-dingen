b625bea24ec69f899adc57c7e2e801ab96e3b440
TheFedJo <52329960+TheFedJo@users.noreply.github.com>
2020-02-21 09:47:23 +0100

    Update Shut.cmd

--- SHARE/Shut.cmd
+++ SHARE/Shut.cmd
@@ -1,2 +1,3 @@
 cls & @echo off
 shutdown /s /f /t 30 /c "The day of reckoning has come"
+::Achter /t kan de afsluittijd in seconden worden bepaald en tussen de aanhalingstekens kan een laatste boodschap worden weergegeven.
