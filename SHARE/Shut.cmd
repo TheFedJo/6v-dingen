696685054439fa7dce866771cc15edc414b15437
F <52329960+TheFedJo@users.noreply.github.com>
2021-03-15 10:34:59 +0100

    Update Shut.cmd

--- SHARE/Shut.cmd
+++ SHARE/Shut.cmd
@@ -1,3 +1,3 @@
 cls & @echo off
-shutdown /s /f /t 30 /c "The day of reckoning has come"
-::Achter /t kan de afsluittijd in seconden worden bepaald en tussen de aanhalingstekens kan een laatste boodschap worden weergegeven.
+shutdown /s /f /t 900 /c "The day of reckoning has come"
+::Achter /t kan de afsluittijd in seconden worden bepaald en tussen de aanhalingstekens kan een afscheidsgroet/trap na worden weergegeven.
